3735bf30c230449f81281c7619fe52ccb7c8d599
Jerry <ngohongphuc2001@gmail.com>
2020-02-28 22:28:45 -0600

    Add partial comments for Driver and LinkedList .java

--- MusicalChair/src/musicalChair/Driver.java
+++ MusicalChair/src/musicalChair/Driver.java
@@ -2,6 +2,13 @@ package musicalChair;
 
 import java.util.*;
 
+/**
+ * @author Jerry Ngo
+ * This class plays the whole musical chair game. 
+ * It asks for the number of people playing the game and how many round it would drop a people.
+ * Then, it creates a new linked list object, plays the game and announces the winner. 
+ */
+
 public class Driver {
 
 	public static void main(String[] args) {
@@ -18,14 +25,10 @@ public class Driver {
 			game.insertAtFrontofList(i);
 		}
 		
-		
 		while(game.sizeOfList != 1) {
 			for(int i = 0; i < k - 1; ++i) game.goToNextElement();
 			game.removeElement();
 		}
-		
 		System.out.format("Player #%d wins the game!", game.getHeadVal());
 	}
-	
-
 }
--- MusicalChair/src/musicalChair/LinkedList.java
+++ MusicalChair/src/musicalChair/LinkedList.java
@@ -1,10 +1,18 @@
 package musicalChair;
 
+/**
+ * This is the circular linked list class
+ */
+
 public class LinkedList {
 
 	private Node headOfList, preHead, tail;
 	int sizeOfList;
 	
+	/**
+	 * @param val the value to input at the front of the linked list
+	 * Add an integer at the front of the linked list
+	 */
 	void insertAtFrontofList(int val) {
 		Node theNewItem = new Node(val, headOfList);
 		if(val == sizeOfList) tail = theNewItem;
@@ -13,6 +21,9 @@ public class LinkedList {
 		if(val == 1) tail.setNext(theNewItem);
 	} 
 	
+	/**
+	 * Move the current head of list to the next item
+	 */
 	void goToNextElement() {
 		preHead = headOfList;
 		headOfList = headOfList.getNext();
